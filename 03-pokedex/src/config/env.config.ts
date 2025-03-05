bc62875fbbf55e994bf7cce26ff1a066610c0699
MarcBgF05 <00028320@uca.edu.sv>
2025-03-04 19:46:06 -0600

    :wastebasket: Remove unused files

--- 03-pokedex/src/config/env.config.ts
+++
@@ -1,8 +0,0 @@
-//Mapeamos variables de entorno a un objeto.
-//Se debe cargar en el módulo de la aplicación, con el método forRoot() y paráemtro load
-export const EnvConfiguration = () => ({
-  environment: process.env.NODE_ENV || 'development',
-  mongodb: process.env.MONGODB,
-  port: process.env.PORT || 3005,
-  defaultLimit: +process.env.DEFAULT_LIMIT || 10,
-});
