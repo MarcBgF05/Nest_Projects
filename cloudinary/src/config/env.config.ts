93975c2292751c89305c3a52b30481b549ccde84
MarcBgF05 <00028320@uca.edu.sv>
2025-03-04 19:20:59 -0600

    :wastebasket: Remove unused files

--- cloudinary/src/config/env.config.ts
+++
@@ -1,6 +0,0 @@
-export const EnvConfiguration = () => ({
-  enviroment: process.env.NODE_ENV || 'development',
-  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
-  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
-  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
-});
--- cloudinary/src/images/cloudinary/cloudinary.storage.ts
+++
@@ -1,6 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-
-export const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-});
